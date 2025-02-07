138bdbc329ee2944b72917c7176fc86c38430651
John Burkhardt <jeb495@drexel.edu>
2025-02-07 12:04:20 -0500

    set up project again

--- app/src/main/java/com/example/appdeal/MainActivity.kt
+++ app/src/main/java/com/example/appdeal/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.example.appdeal
 
-import org.w3c.dom.Text
-import java.lang.reflect.Modifier
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appdeal.ui.theme.AppdealTheme
 
-@androidx.compose.runtime.Composable
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppdealTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
 
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppdealTheme {
+        Greeting("Android")
+    }
 }
